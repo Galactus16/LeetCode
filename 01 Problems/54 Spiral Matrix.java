483577622e08b757405bad669e572271a74aabaa
Anupam Gupta <anupam.gupta@aol.com>
2017-09-11 21:57:34 -0500

    Spiral Matrix
    Spiral Matrix

---
+++ 01 Problems/54 Spiral Matrix.java
@@ -0,0 +1,50 @@
+public class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        
+        if(matrix.length == 0){
+            return result;
+        }
+        
+        int rowBegin = 0;
+        int rowEnd = matrix.length - 1 ;
+        int colBegin = 0;
+        int colEnd = matrix[0].length - 1;
+        
+        while(rowBegin <= rowEnd && colBegin <= colEnd){
+            
+            //Go right
+            for(int j=colBegin; j<=colEnd ; j++){
+                result.add(matrix[rowBegin][j]);
+            }
+            rowBegin++;
+            
+            //Go down
+            for(int j=rowBegin; j <= rowEnd; j++){
+                result.add(matrix[j][colEnd]);
+            }
+            colEnd--;
+            
+            //Go left
+            if(rowBegin <= rowEnd){
+                for(int j=colEnd; j >= colBegin; j--){
+                    result.add(matrix[rowEnd][j]);
+                }
+                rowEnd--;
+            }
+            
+            
+            //Go up
+            if(colBegin <= colEnd){
+                for(int j=rowEnd; j>=rowBegin; j--){
+                    result.add(matrix[j][colBegin]);
+                }
+                colBegin++;
+            }
+        }
+        
+        return result;
+        
+    }
+}
